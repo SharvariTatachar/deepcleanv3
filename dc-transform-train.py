2dbc9004ea320932dff6b1ff9d7682e18ede9954
Sharvari Tatachar <statachar3@login-phoenix-gnr-3.pace.gatech.edu>
2026-03-08 17:35:18 -0400

    config corrections

--- dc-transform-train.py
+++ dc-transform-train.py
@@ -2,6 +2,7 @@ import os
 import torch 
 import pickle
 import argparse
+import configparser
 import logging
 import torch.nn as nn 
 import torch.optim as optim
@@ -14,89 +15,166 @@ import deepclean.model as model
 import deepclean.model.hybrid as hy
 import deepclean.model.utils as utils
 import deepclean.model.deepclean
-import configs.110config as config
+
+
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if isinstance(v, str):
+        return v.lower() in ('yes', 'true', 't', '1')
+    return bool(v)
+
 
 def parse_cmd():
     parser = argparse.ArgumentParser(
         prog=os.path.basename(__file__), usage='%(prog)s [options]')
+
+    parser.add_argument('--config', help='Path to .ini config', type=str,
+                        default='configs/110train.ini')
     
-    # dataset arguments 
-    parser.add_argument('--train-t0', help='GPS of the first sample', type=int)
-    parser.add_argument('--train-duration', help='Duration of train/val frame', type=int)
-    parser.add_argument('--chanslist', help='Path to channel list', type=str)
-    parser.add_argument('--fs', help='Sampling frequency', 
-                        default=config.DEFAULT_SAMPLE_RATE, type=float)
+    # dataset arguments
+    parser.add_argument('--train-t0', help='GPS of the first sample',
+                        type=int, default=None)
+    parser.add_argument('--train-duration', help='Duration of train/val frame',
+                        type=int, default=None)
+    parser.add_argument('--chanslist', help='Path to channel list',
+                        type=str, default=None)
+    parser.add_argument('--fs', help='Sampling frequency',
+                        type=float, default=None)
 
 
-    # preprocess arguments 
-    parser.add_argument('--filt-fl', help="Low frequency for bandpass filter", 
-                        default=config.DEFAULT_FLOW, nargs='+', type=float)
-    parser.add_argument('--filt-fh', help="High frequency for bandpass filter", 
-                        default=config.DEFAULT_FHIGH, nargs='+', type=float)
-    parser.add_argument('--filt-order', help='Bandpass filter order', 
-                        default=config.DEFAULT_FORDER, type=int)
+    # preprocess arguments
+    parser.add_argument('--filt-fl', help="Low frequency for bandpass filter",
+                        nargs='+', type=float, default=None)
+    parser.add_argument('--filt-fh', help="High frequency for bandpass filter",
+                        nargs='+', type=float, default=None)
+    parser.add_argument('--filt-order', help='Bandpass filter order',
+                        type=int, default=None)
+    parser.add_argument('--filt-pad', help='Padding (in seconds) removed after filtering',
+                        type=float, default=5.0)
     
-    # timeseries arguments 
-    parser.add_argument('--train-kernel', help='Length of each segment in seconds', 
-                        default=config.DEFAULT_TRAIN_KERNEL, type=float)
-    parser.add_argument('--train-stride', help='Stride between segments in seconds', 
-                        default=config.DEFAULT_TRAIN_STRIDE, type=float)
-    parser.add_argument('--pad-mode', help='Padding mode', 
-                        default=config.DEFAULT_PAD_MODE, type=str)
+    # timeseries arguments
+    parser.add_argument('--train-kernel', help='Length of each segment in seconds',
+                        type=float, default=None)
+    parser.add_argument('--train-stride', help='Stride between segments in seconds',
+                        type=float, default=None)
+    parser.add_argument('--pad-mode', help='Padding mode',
+                        type=str, default=None)
 
     # training arguments
     parser.add_argument('--batch-size', help='Batch size',
-                        default=config.DEFAULT_BATCH_SIZE, type=int)
+                        type=int, default=None)
     parser.add_argument('--max-epochs', help='Maximum number of epochs to train on',
-                        default=config.DEFAULT_MAX_EPOCHS, type=int)
-    parser.add_argument('--num-workers', help='Number of worker of DataLoader',
-                        default=config.DEFAULT_NUM_WORKERS, type=int)
-    parser.add_argument('--lr', help='Learning rate of ADAM optimizer', 
-                        default=config.DEFAULT_LR, type=float)
+                        type=int, default=None)
+    parser.add_argument('--num-workers', help='Number of workers of DataLoader',
+                        type=int, default=None)
+    parser.add_argument('--lr', help='Learning rate of ADAM optimizer',
+                        type=float, default=None)
     parser.add_argument('--weight-decay', help='Weight decay of ADAM optimizer',
-                        default=config.DEFAULT_WEIGHT_DECAY, type=float)
+                        type=float, default=None)
     
-    # loss function arguments 
+    # loss function arguments
     parser.add_argument('--fftlength', help='FFT length of loss PSD',
-                        default=config.DEFAULT_FFT_LENGTH, type=float)
+                        type=float, default=None)
     parser.add_argument('--overlap', help='Overlapping of loss PSD',
-                        default=config.DEFAULT_OVERLAP, type=float)
+                        type=float, default=None)
 
     parser.add_argument('--psd-weight', help='PSD weight of composite loss',
-                        default=config.DEFAULT_PSD_WEIGHT, type=float)
+                        type=float, default=None)
     parser.add_argument('--mse-weight', help='MSE weight of composite',
-                        default=config.DEFAULT_MSE_WEIGHT, type=float)
+                        type=float, default=None)
     parser.add_argument('--cross-psd-weight', help='Cross-edge PSD weight of comp. loss',
-                        default=config.DEFAULT_CROSS_PSD_WEIGHT, type=float)
-    parser.add_argument('--edge-weight', help='edge weight of composite',
-                        default=config.DEFAULT_EDGE_WEIGHT, type=float)
-    parser.add_argument('--edge-frac', help='fraction of the segment considered as edge',
-                        default=config.DEFAULT_EDGE_FRAC, type=float)
+                        type=float, default=0.0)
+    parser.add_argument('--edge-weight', help='Edge weight of composite',
+                        type=float, default=0.0)
+    parser.add_argument('--edge-frac', help='Fraction of the segment considered as edge',
+                        type=float, default=0.0)
 
-    # input/output arguments 
-    parser.add_argument('--train-dir', help='Path to training directory', 
-                        default='.', type=str)
+    # input/output arguments
+    parser.add_argument('--train-dir', help='Path to training directory',
+                        type=str, default=None)
     parser.add_argument('--filename-training', help='Path to training dataset file (h5)', 
                         default=None, type=str)
-    parser.add_argument('--filename-validation', help='Path to val dataset file (h5)', 
+    parser.add_argument('--filename-validation', help='Path to val dataset file (h5)',
                         default=None, type=str)
     parser.add_argument('--load-dataset', help='Load training dataset',
-                        default=False, type=dc.io.str2bool)
-    parser.add_argument('--save-dataset', help='Save training dataset', 
-                        default=False, type=dc.io.str2bool)
+                        default=None, type=str2bool)
+    parser.add_argument('--save-dataset', help='Save training dataset',
+                        default=None, type=str2bool)
     parser.add_argument('--initial-checkpoint', help='pretrained model to initialize with', 
                         default=None, type=str)
-    parser.add_argument('--log', help='Log file', type=str)
-    
-    # cuda arguments 
-    parser.add_argument('--device', help='Device to use', 
-                        default=config.DEFAULT_DEVICE, type=str)
+    parser.add_argument('--log', help='Log file', type=str, default=None)
+
+    # cuda arguments
+    parser.add_argument('--device', help='Device to use',
+                        type=str, default=None)
     
     params = parser.parse_args()
+
+    # load .ini
+    cfg = configparser.ConfigParser()
+    cfg.read(params.config)
+    c = cfg['config']
+
+    # override missing CLI args from config file
+    if params.train_dir is None and 'train_dir' in c:
+        params.train_dir = c.get('train_dir')
+    if params.log is None and 'log' in c:
+        params.log = c.get('log')
+    if params.device is None and 'device' in c:
+        params.device = c.get('device')
+    if params.train_t0 is None and 'train_t0' in c:
+        params.train_t0 = c.getint('train_t0')
+    if params.train_duration is None and 'train_duration' in c:
+        params.train_duration = c.getint('train_duration')
+    if params.fs is None and 'fs' in c:
+        params.fs = c.getint('fs')
+    if params.chanslist is None and 'chanslist' in c:
+        params.chanslist = c.get('chanslist')
+    if params.train_kernel is None and 'train_kernel' in c:
+        params.train_kernel = c.getfloat('train_kernel')
+    if params.train_stride is None and 'train_stride' in c:
+        params.train_stride = c.getfloat('train_stride')
+    if params.pad_mode is None and 'pad_mode' in c:
+        params.pad_mode = c.get('pad_mode')
+    if params.filt_fl is None and 'filt_fl' in c:
+        # nargs='+' expects a list
+        params.filt_fl = [c.getfloat('filt_fl')]
+    if params.filt_fh is None and 'filt_fh' in c:
+        params.filt_fh = [c.getfloat('filt_fh')]
+    if params.filt_order is None and 'filt_order' in c:
+        params.filt_order = c.getint('filt_order')
+    if params.batch_size is None and 'batch_size' in c:
+        params.batch_size = c.getint('batch_size')
+    if params.max_epochs is None and 'max_epochs' in c:
+        params.max_epochs = c.getint('max_epochs')
+    if params.num_workers is None and 'num_workers' in c:
+        params.num_workers = c.getint('num_workers')
+    if params.lr is None and 'lr' in c:
+        params.lr = c.getfloat('lr')
+    if params.weight_decay is None and 'weight_decay' in c:
+        params.weight_decay = c.getfloat('weight_decay')
+    if params.fftlength is None and 'fftlength' in c:
+        params.fftlength = c.getfloat('fftlength')
+    if params.psd_weight is None and 'psd_weight' in c:
+        params.psd_weight = c.getfloat('psd_weight')
+    if params.mse_weight is None and 'mse_weight' in c:
+        params.mse_weight = c.getfloat('mse_weight')
+    if params.save_dataset is None and 'save_dataset' in c:
+        params.save_dataset = c.getboolean('save_dataset')
+    if params.load_dataset is None and 'load_dataset' in c:
+        params.load_dataset = c.getboolean('load_dataset')
+
+    # final fallbacks
+    if params.train_dir is None:
+        params.train_dir = '.'
+    if params.fs is None:
+        params.fs = 2048
+
     return params 
 
 params = parse_cmd()
-pickle.dump({'params': params}, open('dc_transform_train,p', 'wb'))
+pickle.dump({'params': params}, open('dc_transform_train.p', 'wb'))
 params = pickle.load(open('dc_transform_train.p', 'rb'))['params']
 
 
@@ -104,14 +182,21 @@ os.makedirs(params.train_dir, exist_ok=True)
 if params.log is not None: 
     params.log = os.path.join(params.train_dir, params.log)
 # set up logging to both file and console 
-logging.basicConfig(
-    level=logging.INFO, 
-    format='%(asctime)s - %(message)s',
-    handlers=[
-        logging.FileHandler(params.log, mode='a'),  # Write to file
-        logging.StreamHandler()  # Write to console
-    ]
-)
+if params.log is not None:
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(message)s',
+        handlers=[
+            logging.FileHandler(params.log, mode='a'),  # Write to file
+            logging.StreamHandler()  # Write to console
+        ]
+    )
+else:
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(message)s',
+        handlers=[logging.StreamHandler()]
+    )
 logging.info('Create training directory: {}'.format(params.train_dir))
 
 BS = 8
@@ -139,7 +224,7 @@ val_data = val_data.bandpass(params.filt_fl, params.filt_fh, params.filt_order, 
 
 # filter pad default from deepclean-prod, is 5: 
 
-train_data.data = train_data.data[:, int(params.filt_pad * params.fs):-int(params.filt_pad * param.fs)]
+train_data.data = train_data.data[:, int(params.filt_pad * params.fs):-int(params.filt_pad * params.fs)]
 val_data.data = val_data.data[:, int(params.filt_pad * params.fs):-int(params.filt_pad * params.fs)]
 # test_data.data = test_data.data[:, int(filt_pad * fs):-int(filt_pad * fs)]
 
@@ -195,19 +280,19 @@ criterion = dc.criterion.CompositePSDLoss(
     fl=params.filt_fl,
     fh=params.filt_fh,
     fftlength=params.fftlength,
-    overlap=None,
+    overlap=params.overlap,
     psd_weight=params.psd_weight,
-    mse_weight=params.mse_weights,
+    mse_weight=params.mse_weight,
     reduction='mean',
     device=device,
     average='mean'
 )
 
-optimizer = optim.Adam(model.parameters(), lr = params.lr, weight_decay=params.weight_decay)
+optimizer = optim.Adam(model.parameters(), lr=params.lr, weight_decay=params.weight_decay)
 
 lr_scheduler = optim.lr_scheduler.StepLR(optimizer, 10, 0.1)
 
-train_logger = dc.logger.Logger(outdir=train_dir, metrics=['loss'])
+train_logger = dc.logger.Logger(outdir=params.train_dir, metrics=['loss'])
 utils.train(
     train_loader, model, criterion, device, optimizer, lr_scheduler, 
     val_loader=val_loader, max_epochs=params.max_epochs, logger=train_logger)
